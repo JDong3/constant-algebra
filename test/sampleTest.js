4cad2cb443bbbc4a088f4044b14d5f56fbea1035
xxcandiez <josephdong1998@gmail.com>
2017-08-12 01:33:14 -0400

    created new unittesting workflow

---
+++ test/sampleTest.js
@@ -0,0 +1,25 @@
+const chai = require("chai")
+const describe = require("mocha").describe
+const it = require("mocha").it
+let assert = chai.assert
+
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = []
+    assert.equal(arr.length, 0)
+  })
+})
+
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = []
+    assert.equal(arr.length, 0)
+  })
+})
+
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = []
+    assert.equal(arr.length, 1)
+  })
+})
--- unittests/matrix__bool_tests.js
+++
@@ -1,5 +0,0 @@
-const chai = require("chai")
-
-var assert = chai.assert
-
-assert.equal(1, 0, "ok boys")
