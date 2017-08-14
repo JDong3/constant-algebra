52ef538c4aed42f1411da8509c2b739ccf6d99e5
xxcandiez <josephdong1998@gmail.com>
2017-08-14 14:14:31 -0400

    refactored unittests for matrix__bool.js

--- test/matrix__bool_test.js
+++ test/matrix__bool_test.js
@@ -4,69 +4,95 @@ const describe = require('mocha').describe
 const it = require('mocha').it
 let assert = chai.assert
 
+let matrices = {
+  // special input
+  specVerifyNonRectangularity: [[0, 0, 0],
+                                [0, 0]],
+  specZvector3: [0, 0, 0],
+  specVerifyAlmostArrayOfArray: [[0, 0, 0],
+                                 0,
+                                 [0, 0, 0]],
+  specEmpty11: [[]],
+  specEmpty30: [[],
+                [],
+                []],
+  specPartialIdentity23: [[1, 0, 0],
+                          [0, 1, 0]],
+  // zero matrices
+  z11: [[0]],
+  z12: [[0, 0]],
+  z13: [[0, 0, 0]],
+  z21: [[0],
+        [0]],
+  z22: [[0, 0],
+        [0, 0]],
+  z23: [[0, 0, 0],
+        [0, 0, 0]],
+  z32: [[0, 0],
+        [0, 0],
+        [0, 0]],
+  z33: [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]],
+  z34: [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]],
+  z43: [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]],
+  // identity matrices
+  i11: [[1]],
+  i22: [[1, 0],
+        [0, 1]],
+  i33: [[1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]]
+  
+}
+
 describe('Verify', function() {
   describe('accepts rectangularity', function() {
     it('accepts 1x1 small square', function() {
-      let m = [[0]]
-      assert.isTrue(mb.verify(m), 'a 1x1 square is a rectangle')
+      assert.isTrue(mb.verify(matrices.z11), 'a 1x1 square is a rectangle')
     }),
     it('accepts 2x2 larger square', function() {
-      let m = [[0, 0],
-              [0, 0]]
-      assert.isTrue(mb.verify(m), 'a 2x2 square is a rectangle')
+      assert.isTrue(mb.verify(matrices.z22), 'a 2x2 square is a rectangle')
     }),
     it('accepts 3x3 largerer square', function() {
-      let m = [[0, 0, 0],
-               [0, 0, 0],
-               [0, 0, 0]]
-      assert.isTrue(mb.verify(m))
+      assert.isTrue(mb.verify(matrices.z33))
     }),
     it('accepts 3x2 rectangle', function() {
-      let m = [[0, 0],
-               [0, 0],
-               [0, 0]]
-      assert.isTrue(mb.verify(m))
+      assert.isTrue(mb.verify(matrices.z32))
     }),
     it('accepts 2x3 rectangle', function() {
-      let m = [[0, 0, 0],
-               [0, 0, 0]]
-      assert.isTrue(mb.verify(m))
+      assert.isTrue(mb.verify(matrices.z23))
     })
   }),
   describe('rejects non-rectangularity', function() {
     it('rejects different size rows', function() {
-      let m = [[0, 0, 0],
-               [0, 0]]
-      assert.isFalse(mb.verify(m))
+      assert.isFalse(mb.verify(matrices.specVerifyNonRectangularity))
     })
   })
   describe('accepts array of array', function() {
     it('accepts base case', function() {
-      let m = [[0]]
-      assert.isTrue(mb.verify(m))
+      assert.isTrue(mb.verify(matrices.z11))
     })
   })
   describe('rejects array of not array', function() {
     it('rejects array of int', function() {
-      let m = [0, 0, 0]
-      assert.isFalse(mb.verify(m))
+      assert.isFalse(mb.verify(matrices.specZvector3))
     }),
     it('rejects array of mostly arrays but one int', function() {
-      let m = [[0, 0, 0],
-               0,
-               [0, 0, 0]]
-      assert.isFalse(mb.verify(m))
+      assert.isFalse(mb.verify(matrices.verifyAlmostArrayOfArray))
     })
   })
   describe("rejects size 0", function() {
     it('rejects empty case', function() {
-      let m = [[]]
+      assert.isFalse(mb.verify(matrices.specEmpty11))
     }),
     it('rejects width of 0', function() {
-      let m = [[],
-               [],
-               []]
-      assert.isFalse(mb.verify(m))
+      assert.isFalse(mb.verify(matrices.specEmpty30))
     })
   })
 })
@@ -74,31 +100,21 @@ describe('Verify', function() {
 describe('isSquare', function() {
   describe('accepts square(nxn) matrices', function() {
     it('accepts 1x1 square matrix', function() {
-      let m = [[0]]
-      assert.isTrue(mb.isSquare(m))
+      assert.isTrue(mb.isSquare(matrices.z11))
     })
     it('accepts 3x3 square matrix', function() {
-      let m = [[0, 0, 0],
-               [0, 0, 0],
-               [0, 0, 0]]
-      assert.isTrue(mb.isSquare(m))
+      assert.isTrue(mb.isSquare(matrices.z33))
     })
   }),
   describe('rejects non square(nxm, where n!=m) matrices', function() {
     it('rejects 1x2 matrix', function(){
-      let m = [[0, 1]]
-      assert.isFalse(mb.isSquare(m))
+      assert.isFalse(mb.isSquare(matrices.z12))
     }),
     it('rejects 2x1 matrix', function() {
-      let m = [[0],
-               [1]]
-      assert.isFalse(mb.isSquare(m))
+      assert.isFalse(mb.isSquare(matrices.z21))
     }),
     it('rejects 3x4 matrix', function() {
-      let m = [[0, 1, 2, 3],
-               [0, 1, 2, 3],
-               [0, 1, 2, 3]]
-      assert.isFalse(mb.isSquare(m))
+      assert.isFalse(mb.isSquare(matrices.z34))
     })
   })
 })
@@ -106,41 +122,27 @@ describe('isSquare', function() {
 describe('isIdentity', function() {
   describe('accepts identity matrices', function() {
     it('accepts size 1 identity', function() {
-      let m = [[1]]
-      assert.isTrue(mb.isIdentity(m))
+      assert.isTrue(mb.isIdentity(matrices.i11))
     }),
     it('accepts size 2 identity', function() {
-      let m = [[1, 0],
-               [0, 1]]
-      assert.isTrue(mb.isIdentity(m))
+      assert.isTrue(mb.isIdentity(matrices.i22))
     }),
     it('accepts size 3 identity', function() {
-      let m = [[1, 0, 0],
-               [0, 1, 0],
-               [0, 0, 1]]
-      assert.isTrue(mb.isIdentity(m))
+      assert.isTrue(mb.isIdentity(matrices.i33))
     })
   }),
   describe('rejects non identity matrices', function() {
     it('rejects 1x1 0 matrix', function() {
-      let m = [[0]]
-      assert.isFalse(mb.isIdentity(m))
+      assert.isFalse(mb.isIdentity(matrices.z11))
     }),
     it('rejects 2x2 0 matrix', function() {
-      let m = [[0, 0],
-               [0, 0]]
-      assert.isFalse(mb.isIdentity(m))
+      assert.isFalse(mb.isIdentity(matrices.z22))
     }),
-    it('rejects 3x3 almost identity', function() {
-      let m = [[1, 0, 0],
-               [0, 1, 0],
-               [0, 0, 0]]
-      assert.isFalse(mb.isIdentity(m))
+    it('rejects 3x3 0 matrix', function() {
+      assert.isFalse(mb.isIdentity(matrices.z33))
     }),
     it('rejects non square matrix', function() {
-      let m = [[1, 0, 0],
-               [0, 1, 0]]
-      assert.isFalse(mb.isIdentity(m))
+      assert.isFalse(mb.isIdentity(matrices.specPartialIdentity23))
     })
   })
 })
@@ -149,44 +151,24 @@ describe('isIdentity', function() {
 describe('addDefined', function() {
   describe('accepts matrices of the same size', function(){
     it('accepts 1x1 and 1x1 matrices', function() {
-      let m1 = [[0]]
-      let m2 = [[0]]
-      assert.isTrue(mb.addDefined(m1, m2))
+      assert.isTrue(mb.addDefined(matrices.z11, matrices.z11))
     }),
     it('accepts 3x3 and 3x3 matrices', function() {
-      let m1 = [[0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0]]
-      let m2 = [[0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0]]
-      assert.isTrue(mb.addDefined(m1, m2))
+      assert.isTrue(mb.addDefined(matrices.z33, matrices.z33))
     }),
     it('accepts 2x3 and 2x3 matrices', function() {
-      let m1 = [[0, 0, 0],
-                [0, 0, 0]]
-      let m2 = [[0, 0, 0],
-                [0, 0, 0]]
-      assert.isTrue(mb.addDefined(m1, m2))
+      assert.isTrue(mb.addDefined(matrices.z23, matrices.z23))
     })
   }),
   describe('rejects matrices of different sizes', function() {
     it('rejects 1x1 and 2x2 matrices', function(){
-      let m1 = [[0]]
-      let m2 = [[0, 0],
-                [0, 0]]
-      assert.isFalse(mb.addDefined(m1, m2))
+      assert.isFalse(mb.addDefined(matrices.z11, matrices.z22))
     }),
     it('rejects 1x1 and 1x2 matrices', function() {
-      let m1 = [[0]]
-      let m2 = [[0, 0]]
-      assert.isFalse(mb.addDefined(m1, m2))
+      assert.isFalse(mb.addDefined(matrices.z11, matrices.z12))
     }),
     it('rejects 1x1 and 2x1 matrices', function() {
-      let m1 = [[0]]
-      let m2 = [[0],
-                [0]]
-      assert.isFalse(mb.addDefined(m1, m2))
+      assert.isFalse(mb.addDefined(matrices.z11, matrices.z21))
     })
   })
 })
@@ -194,91 +176,62 @@ describe('addDefined', function() {
 describe('subDefined', function() {
   describe('accepts matrices of the same size', function(){
     it('accepts 1x1 and 1x1 matrices', function() {
-      let m1 = [[0]]
-      let m2 = [[0]]
-      assert.isTrue(mb.addDefined(m1, m2))
+      assert.isTrue(mb.subDefined(matrices.z11, matrices.z11))
     }),
     it('accepts 3x3 and 3x3 matrices', function() {
-      let m1 = [[0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0]]
-      let m2 = [[0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0]]
-      assert.isTrue(mb.addDefined(m1, m2))
+      assert.isTrue(mb.subDefined(matrices.z33, matrices.z33))
     }),
     it('accepts 2x3 and 2x3 matrices', function() {
-      let m1 = [[0, 0, 0],
-                [0, 0, 0]]
-      let m2 = [[0, 0, 0],
-                [0, 0, 0]]
-      assert.isTrue(mb.addDefined(m1, m2))
+      assert.isTrue(mb.subDefined(matrices.z23, matrices.z23))
     })
   }),
   describe('rejects matrices of different sizes', function() {
     it('rejects 1x1 and 2x2 matrices', function(){
-      let m1 = [[0]]
-      let m2 = [[0, 0],
-                [0, 0]]
-      assert.isFalse(mb.addDefined(m1, m2))
+      assert.isFalse(mb.subDefined(matrices.z11, matrices.z22))
     }),
     it('rejects 1x1 and 1x2 matrices', function() {
-      let m1 = [[0]]
-      let m2 = [[0, 0]]
-      assert.isFalse(mb.addDefined(m1, m2))
+      assert.isFalse(mb.subDefined(matrices.z11, matrices.z12))
     }),
     it('rejects 1x1 and 2x1 matrices', function() {
-      let m1 = [[0]]
-      let m2 = [[0],
-                [0]]
-      assert.isFalse(mb.addDefined(m1, m2))
+      assert.isFalse(mb.subDefined(matrices.z11, matrices.z21))
     })
   })
 })
 
 describe('rowValid accepts rows int r in {0..n-1}, for a nxa matrix', function() {
-  describe('accepts rows r in {0}, for a 1x1 matrix', function() {
+  describe('accepts rows in {0} for a 1x1 matrix', function() {
     it('accepts 0', function() {
-      let m = [[0]]
-      assert.isTrue(mb.rowValid(m, 0))
+      assert.isTrue(mb.rowValid(matrices.z11, 0))
     })
   }),
-  describe('rejects rows r in I\\{0}, for a 1x1 matrix', function() {
+  describe('rejects rows in I\\{0} for a 1x1 matrix', function() {
     it('rejects -1', function() {
-      let m = [[0]]
-      assert.isFalse(mb.rowValid(m, -1))
+      assert.isFalse(mb.rowValid(matrices.z11, -1))
     }),
     it('rejects 1', function() {
-      let m = [[0]]
-      assert.isFalse(mb.rowValid(m, 1))
+      assert.isFalse(mb.rowValid(matrices.z11, 1))
     })
-    
+  })
+  describe('accepts rows in {0, 1, 2} for a 3x3 matrix', function() {
+    it('accepts 0', function() {
+      assert.isTrue(mb.rowValid(matrices.z33, 0))
+    }),
+    it('accepts 1', function() {
+      assert.isTrue(mb.rowValid(matrices.z33, 1))
+    }),
+    it('accepts 2', function() {
+      assert.isTrue(mb.rowValid(matrices.z33, 2))
+    })
+  }),
+  describe('rejects rows I\\{0, 1, 2} for a 3x3 matrix', function() {
+    it('rejects -1', function() {
+      assert.isFalse(mb.rowValid(matrices.z33, -1))
+    }),
+    it('rejects 3', function() {
+      assert.isFalse(mb.rowValid(matrices.z33, 3))
+    })
   })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
