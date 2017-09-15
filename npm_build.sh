2fc27e3d7f5d73688758609f6e362208ea24ef14
xxcandiez <josephdong1998@gmail.com>
2017-09-15 16:30:12 -0400

    npm build working

---
+++ npm_build.sh
@@ -0,0 +1,4 @@
+npm run build
+cp ./index.js ./npm_build/index.js
+cp ./package.json ./npm_build/package.json
+cp ./package-lock.json ./npm_build/package-lock.json
--- npm_build/index.js
+++ npm_build/index.js
@@ -1,10 +1,42 @@
-'use strict';
+const src = require('./src')
 
-var lib = require('./lib.js');
-var res = require('./res.js');
-var ver = require('./ver.js');
-var util = require('./util.js');
+const mb = src.lib.mb
+const mm = src.lib.mm
+const mn = src.lib.mn
+const mv = src.lib.mv
+const vb = src.lib.vb
+const vn = src.lib.vn
+const vv = src.lib.vv
+const vectors = src.res.vectors
+const matrices = src.res.matrices
+const util = src.util
+
+const is = src.ver.is
+const mbv = src.ver.mbv
+const mmv = src.ver.mmv
+const mnv = src.ver.mnv
+const mvv = src.ver.mvv
+const vbv = src.ver.vbv
+const vnv = src.ver.vnv
+const vvv = src.ver.vvv
 
 module.exports = {
-  lib: lib, res: res, ver: ver, util: util
-};
+  is,
+  mb,
+  mm,
+  mn,
+  mv,
+  vb,
+  vn,
+  vv,
+  vectors,
+  matrices,
+  util,
+  mbv,
+  mmv,
+  mnv,
+  mvv,
+  vbv,
+  vnv,
+  vvv
+}
